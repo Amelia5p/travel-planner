de397fb4064a49fab586ac0fccd0765f9e6a609b
Amelia <millie-service@hotmail.com>
2025-02-18 16:55:52 +0000

    style signup page, update redirection in html, add login required to user views

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
 # Sign up view, if the form is valid the user will be saved,
 # Automatically logged in, redirected to profile page.
+
 
 def signup(request):
     if request.method == "POST":
@@ -15,10 +17,12 @@ def signup(request):
             user = form.save()
             login(request, user)
             return redirect('profile')
-        else:
-            form = UserCreationForm()
-        return render(request, 'users/signup.html')
+    else:
+        form = UserCreationForm()
     
+    return render(request, 'users/signup.html', {'form': form})  
 
+@login_required 
 def profile(request):
     return render(request, 'users/profile.html')
+
