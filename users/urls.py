c1ff5623bca225aebda91b3960cc1105840f30a8
Amelia <millie-service@hotmail.com>
2025-02-18 11:12:31 +0000

    link bootstrap to base.html

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='uses/logout.html', name= 'logout')),
+     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('signup/', views.signup, name = 'signup'),
     path('profile/', views.profile, name='profile'),
 ] 
