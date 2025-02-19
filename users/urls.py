60315db8cfb3cfe88fc8a157eb6657081cf8e96b
Amelia <millie-service@hotmail.com>
2025-02-19 10:58:03 +0000

    style buttons to match rest on profile page, delete profile view

--- users/urls.py
+++ users/urls.py
@@ -5,9 +5,11 @@ from . import views
 
 urlpatterns = [
 
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    path('signup/', views.signup, name = 'signup'),
     path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/delete/', views.delete_profile, name='delete_profile'),
+    path('signup/', views.signup, name = 'signup'),
+    
 ] 
