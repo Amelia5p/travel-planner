1e78cd4a932a711eba5f5fb271b4478d614a5dbe
Amelia <millie-service@hotmail.com>
2025-03-03 11:34:07 +0000

    add first form class

--- trips/admin.py
+++ trips/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from .models import Trip, TripLocation, TripDetails, TripBudget, ItineraryDay
+from .models import Trip, TripLocation, TripDetails, TripBudget, ItineraryDay
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
---
+++ trips/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from models import Trip
+from datetime import timedelta
+
+
+class tripDetails(forms.modelForm):
+    class Meta:
+          model = Trip
+          fields= ['name', 'start_date', 'end_date']
+          widgets= {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'type': 'date'}),
+          }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        
+        if start_date and end_date and end_date < start_date:
+            raise forms.ValidationError("End date cannot be before start date")
+        
+        return cleaned_data
